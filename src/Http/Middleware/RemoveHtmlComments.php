91faa04a17ac33f487a22cd3a74d9ff98c960bba
Patrick Delz <patrick@diffrent.ch>
2025-01-10 16:25:43 +0100

    Updated some code

--- src/Http/Middleware/RemoveHtmlComments.php
+++ src/Http/Middleware/RemoveHtmlComments.php
@@ -25,4 +25,3 @@ class RemoveHtmlComments
         return $response;
     }
 }
-}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,11 +7,9 @@ use Diffrentdigital\RemoveHtmlComments\Http\Middleware\RemoveHtmlComments;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    public function boot()
-    {
-        parent::boot();
-
-        // Register middleware for the `web` group
-        $this->app['router']->pushMiddlewareToGroup('web', RemoveHtmlComments::class);
-    }
+    protected $middlewareGroups = [
+        'web' => [
+            RemoveHtmlComments::class,
+        ],
+    ];
 }
